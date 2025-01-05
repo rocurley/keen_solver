239a5bd47e699f14feb15a36c9a83114a10a32cd
Roger Curley <rocurley@gmail.com>
2025-01-05 09:14:29 -0500

    Another 2x speedup from using a smarter vectorization

--- src/radial_search.rs
+++ src/radial_search.rs
@@ -1,9 +1,9 @@
 use std::{
-    ops::Range,
-    simd::{cmp::SimdPartialEq, LaneCount, Mask, Simd, SupportedLaneCount},
+    iter::zip,
+    simd::{cmp::SimdPartialEq, LaneCount, Simd, SupportedLaneCount},
 };
 
-use crate::game::{Bitmask, BlockInfo, GameState};
+use crate::game::{BlockInfo, GameState};
 
 struct SearchBlock<'a> {
     block: &'a BlockInfo,
@@ -30,8 +30,7 @@ where
 {
     block: &'a BlockInfo,
     possibility_ix: usize,
-    interactions: Vec<<Simd<Bitmask, LANES> as SimdPartialEq>::Mask>,
-    cell_range: Range<usize>,
+    possibility_masks: Vec<Simd<u64, LANES>>,
 }
 
 impl<'a, const LANES: usize> SearchBlockSimd<'a, LANES>
@@ -41,12 +40,8 @@ where
     fn can_increment(&self) -> bool {
         self.possibility_ix + 1 < self.block.possibilities.len()
     }
-    fn current_possibility(&self) -> &[i8] {
-        &self.block.possibilities[self.possibility_ix]
-    }
-    fn write_possibility(&self, cells: &mut Simd<i8, LANES>) {
-        let target = &mut cells.as_mut_array()[self.cell_range.clone()];
-        target.clone_from_slice(self.current_possibility());
+    fn current_possibility_mask(&self) -> Simd<u64, LANES> {
+        self.possibility_masks[self.possibility_ix]
     }
 }
 
@@ -154,13 +149,13 @@ impl GameState {
             };
             search_space.push(sb);
         }
-        let cells_count: usize = search_space.iter().map(|sb| sb.block.cells.len()).sum();
+
         if vectorize {
-            match cells_count {
-                0..=8 => search_vectorized::<8>(search_space),
-                9..=16 => search_vectorized::<16>(search_space),
-                17..=32 => search_vectorized::<32>(search_space),
-                _ => search_unvectorized(search_space),
+            match 2 * self.size * self.size {
+                0..=64 => search_vectorized::<1>(self.size, search_space),
+                65..=128 => search_vectorized::<2>(self.size, search_space),
+                129..=256 => search_vectorized::<4>(self.size, search_space),
+                _ => panic!("Size {} larger than expected", self.size),
             }
         } else {
             search_unvectorized(search_space)
@@ -198,52 +193,56 @@ fn search_unvectorized(mut search_space: Vec<SearchBlock<'_>>) -> bool {
     }
 }
 
-fn search_vectorized<const LANES: usize>(old_search_space: Vec<SearchBlock<'_>>) -> bool
+fn search_vectorized<const LANES: usize>(
+    size: usize,
+    old_search_space: Vec<SearchBlock<'_>>,
+) -> bool
 where
     LaneCount<LANES>: SupportedLaneCount,
 {
     let mut search_space = Vec::<SearchBlockSimd<LANES>>::new();
-    let mut start = 0;
-    let mut cells = Simd::<i8, LANES>::splat(-1);
     for SearchBlock {
         block,
         possibility_ix,
-        interactions,
+        interactions: _,
     } in old_search_space
     {
-        let mut simd_interactions = vec![Mask::splat(false); block.cells.len()];
-        for interaction in interactions {
-            let other_block = &search_space[interaction.other_block_ix];
-            let i = other_block.cell_range.start + interaction.other_cell_ix;
-            simd_interactions[interaction.local_cell_ix].set(i, true);
-        }
-        let end = start + block.cells.len();
-        let cell_range = start..end;
-        start = end;
+        let possibility_masks = block
+            .possibilities
+            .iter()
+            .map(|p| {
+                let mut mask = Simd::<u64, LANES>::splat(0);
+                for (&value, ix) in zip(p, &block.cells) {
+                    let i = (ix % size) * size + value as usize - 1;
+                    mask.as_mut_array()[i / 64] |= 1 << (i % 64);
+                    let i = (size + ix / size) * size + value as usize - 1;
+                    mask.as_mut_array()[i / 64] |= 1 << (i % 64);
+                }
+                mask
+            })
+            .collect();
         let sb = SearchBlockSimd {
             block,
             possibility_ix,
-            interactions: simd_interactions,
-            cell_range,
+            possibility_masks,
         };
-        sb.write_possibility(&mut cells);
         search_space.push(sb);
     }
     loop {
+        let mut seen = search_space[0].current_possibility_mask();
         let validation_failure = (1..search_space.len()).find(|&i| {
             let r_block = &search_space[i];
-            r_block
-                .interactions
-                .iter()
-                .zip(r_block.cell_range.clone())
-                .any(|(interaction, cell_i)| {
-                    let cell_value = Simd::splat(cells[cell_i]);
-                    let conflicts = *interaction & cell_value.simd_eq(cells);
-                    conflicts.any()
-                })
+            let r_mask = r_block.current_possibility_mask();
+            if (r_mask & seen).simd_ne(Simd::splat(0)).any() {
+                return true;
+            }
+            seen |= r_mask;
+            false
         });
         let mut increment_point = match validation_failure {
-            None => return true,
+            None => {
+                return true;
+            }
             Some(x) => x,
         };
         // There's a conflict between search_space[increment_point] and some block in
@@ -255,10 +254,8 @@ where
             return false;
         }
         search_space[increment_point].possibility_ix += 1;
-        search_space[increment_point].write_possibility(&mut cells);
         for block in &mut search_space[increment_point + 1..] {
             block.possibility_ix = 0;
-            block.write_possibility(&mut cells);
         }
     }
 }
@@ -267,18 +264,38 @@ where
 mod tests {
     use crate::GameState;
     #[test]
-    fn test_radial_search_vectorization() {
+    fn test_vectorization_small() {
         let path = "test_data/radial_input";
+        single_vectorization_test(path);
+    }
+    #[ignore]
+    #[test]
+    fn test_vectorization_big() {
+        for i in 0..=80 {
+            let path = format!("hard_examples/{:04}a", i);
+            single_vectorization_test(&path);
+        }
+    }
+
+    fn single_vectorization_test(path: &str) {
         let save = std::fs::read(&path).unwrap();
         let mut expected = GameState::from_save(save.as_slice());
+        let mut actual = expected.clone();
         let (block_id, _) = expected.most_interesting_block();
         let expected_progressed = expected.radial_search_single(block_id, false);
-        let mut actual = expected.clone();
         let actual_progressed = actual.radial_search_single(block_id, true);
         assert_eq!(
             expected, actual,
             "{}\nexpected_progressed: {}\nactual_progressed: {}",
             path, expected_progressed, actual_progressed
         );
+        /*
+        let mut expected_bytes = Vec::new();
+        expected.write_save(&mut expected_bytes);
+        let mut actual_bytes = Vec::new();
+        actual.write_save(&mut actual_bytes);
+        assert_eq!(expected_bytes, actual_bytes);
+        */
+        assert_eq!(actual_progressed, expected_progressed);
     }
 }
